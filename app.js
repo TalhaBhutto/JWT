0ad7c8e7f029ca6f7748a6e69f22c37353ae0249
TalhaBhutto <74734659+TalhaBhutto@users.noreply.github.com>
2021-04-10 22:26:09 +0500

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,12 @@ const app=exp();
 //importing modal Login
 const Login=require('./Model/Login');
 //import body parser to post data
-var bodyParser=require('body-parser')
+var bodyParser=require('body-parser');
+var jsonParser=bodyParser.json();
+//import crypto to encrypt the password
+var crypto=require('crypto');
+var key="password";
+var algo="aes256";
 //import mongoose for connecting to mongo db
 const mongo=require('mongoose');
 mongo.connect('mongodb+srv://Talha:lYDxLcpsmAs2gC0Y@cluster0.gmnqm.mongodb.net/LearningMongo?retryWrites=true&w=majority'
@@ -18,4 +23,13 @@ mongo.connect('mongodb+srv://Talha:lYDxLcpsmAs2gC0Y@cluster0.gmnqm.mongodb.net/L
 app.get("/",(req,res)=>{
     res.end("hello");
 });
+
+app.post('/register',jsonParser,(req,res)=>{
+    var cipher=crypto.createCipher(algo,key);
+    var encrypted=cipher.update(req.body.password,'utf-8','hex');
+
+    console.warn(req.body.encrypted);
+    res.end("hello");
+})
+
 app.listen(420);
