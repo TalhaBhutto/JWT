0f653253e0a838f782f34a2550549ced4e2fe2ed
TalhaBhutto <74734659+TalhaBhutto@users.noreply.github.com>
2021-04-10 23:57:06 +0500

    Update app.js

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ app.post('/register', jsonParser, (req, res) => {
         })
 })
 app.post('/login',jsonParser,(req,res)=>{
-    Login.findOne({email:req})
+    Login.findOne({email:req.body})
 
 })
 app.listen(420);
